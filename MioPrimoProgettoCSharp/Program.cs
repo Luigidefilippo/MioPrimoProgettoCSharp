d6038d9f93b2887d63dd6856d8011a10c2851811
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-05 14:03:52 +0200

    Aggiungere i file di progetto.

---
+++ MioPrimoProgettoCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bella a tutti ragazzi sono ciccogamer89");
+Console.WriteLine("Come ti chiami paguro ?");
+
+var nome = Console.ReadLine();
+
+Console.WriteLine("Benvenuto " + nome + ", piacere di conoscerti !");
